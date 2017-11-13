d914492003140032627b111c694c8a49bf96322a
prateek <whyprateeksharma@gmail.com>
2017-11-13 02:46:16 -0800

    adding mocha setup

---
+++ kafkaend/test.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var request = require('supertest');
+var assert = require('chai').assert;
+
+
+var random = Math.random();
+var tokem = '';
+
+
+it('should respond with success flag on', function(done) {
+  //
+});
