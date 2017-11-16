6449afd362769b57d08a89f4d59a5a90c7676df3
prateek <whyprateeksharma@gmail.com>
2017-11-16 11:52:25 -0800

    unfinished commit

--- kafkaend/services/auth.js
+++ kafkaend/services/auth.js
@@ -5,52 +5,49 @@ var bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 function signin(msg, callback){
-    var res = {};
-    if(validator.isEmail(msg.email) && validator.isByteLength(msg.password, {min: 5})) {
-        var connection = mysql.getConnection();
-        console.log(connection);
-        connection.connect(function(err) {
-            if (err) throw err;
-            console.log("Connected!");
-            connection.query("select id, username, email, role, password from auth_user where email =" + "'" + msg.email + "';", function (err, result) {
-                connection.end();
-                if (err) throw err;
-                if(result[0]) {
-                    var user = result[0];
-                    if(user.role != msg.role) {
-                        res.code = 401;
-                        res.message = "Unauthorized user";
-                        callback(null, res);
-                    }
-                    bcrypt.compare(msg.password, user.password, function(err,result) {
-                            if(result) {
-                                res.code = 200;
-                                res.message = "Success";
-                                res.data = {_id:user.id,username:user.username,role:user.role};
-                                console.log(res);
-                            } else {
-                                res.code = 401;
-                                res.message = "Invalid password";
-                            }
-                            callback(null, res);
-                        });
-                } else {
-                    res.code = 401;
-                    res.message = "Invalid email";
-                    callback(null, res);
-                }
-            });
-        });
-    } else {
-        res.code = 400;
-        res.message = "Fields did not pass the validation";
-        callback(null, res);
-    }
+  var res = {};
+  if(validator.isEmail(msg.email) && validator.isByteLength(msg.password, {min: 5})) {
+    var connection = mysql.getConnection(function(err) {
+      if (err) throw err;
+      console.log("Connected!");
+        mysql.query("select id, username, email, role, password from auth_user where email =" + "'" + msg.email + "';", function (err, result) {
+        if (err) throw err;
+        if(result[0]) {
+          var user = result[0];
+          if(user.role != msg.role) {
+            res.code = 401;
+            res.message = "Unauthorized user";
+            callback(null, res);
+          }
+          bcrypt.compare(msg.password, user.password, function(err,result) {
+            if(result) {
+              res.code = 200;
+              res.message = "Success";
+              res.data = {_id:user.id,username:user.username,role:user.role};
+              console.log(res);
+            } else {
+              res.code = 401;
+              res.message = "Invalid password";
+            }
+            callback(null, res);
+          });
+        } else {
+          res.code = 401;
+          res.message = "Invalid email";
+          callback(null, res);
+        }
+      });
+    });
+  } else {
+    res.code = 400;
+    res.message = "Fields did not pass the validation";
+    callback(null, res);
+  }
 }
 
 function signup(msg, callback){
     var res = {};
-    if(msg.email && msg.email !== '' 
+    if(msg.email && msg.email !== ''
         && msg.password && msg.password !== '') {
         //TODO: change to MySQL
         mongo.getCollection('user', function(err,coll){
--- kafkaend/services/mysql.js
+++ kafkaend/services/mysql.js
@@ -1,14 +1,80 @@
+// var mysql = require('mysql');
+// var config = require('config');
+//
+// function getConnection() {
+// 	var con = mysql.createConnection({
+//   		host: config.mySqlHost,
+//   		user: config.mySqlUsername,
+//   		password: config.mySqlPassword,
+//   		database: config.mySqlDb
+// 	});
+// 	return con;
+// }
+//
+// exports.getConnection = getConnection;
 var mysql = require('mysql');
-var config = require('config');
 
-function getConnection() {
-	var con = mysql.createConnection({
-  		host: config.mySqlHost,
-  		user: config.mySqlUsername,
-  		password: config.mySqlPassword,
-  		database: config.mySqlDb
+var numberOfConnection = 151;
+var connectionStack = [];
+var connectionQueue = [];
+
+var createConnectionPool = function(numberOfConnection){
+
+	//console.log("creating connection pool...");
+	var conn;
+	for(var count=0; count < numberOfConnection; count++){
+		conn = mysql.createConnection({
+			host : 'localhost',
+			user : 'root',
+			password : 'root',
+			database : 'kayak',
+			port : 3306
+		});
+		connectionStack.push(conn);
+	}
+}
+
+var getConnection = function(callback){
+
+	if(connectionStack.length > 0){
+		//connection available, pop from the connection stack
+		connection = connectionStack.pop();
+		callback(null, connection);
+	} else {
+		//connection not available, push to connection wait queue
+		connectionQueue.push(callback);
+	}
+}
+
+//check for available connection every second
+setInterval(function(){
+	if(connectionStack.length > 0){
+		if(connectionQueue.length > 0){
+			callback = connectionQueue.shift();
+			connection = connectionStack.pop();
+			callback(null, connection);
+		}
+	}
+}, 1000)
+
+
+function query(sqlQuery, callback) {
+
+	//console.log("\nSQL Query:" + sqlQuery);
+
+	getConnection(function(err, connection) {
+		connection.query(sqlQuery, function(err, result) {
+			if (err) {
+				//console.log("ERROR: " + err.message);
+			} else {
+				connection.releaseConnection;
+				connectionStack.push(connection);
+				callback(err, result);
+			}
+		});
 	});
-	return con;
 }
 
 exports.getConnection = getConnection;
+exports.createConnectionPool =  createConnectionPool;
+exports.query=query;
