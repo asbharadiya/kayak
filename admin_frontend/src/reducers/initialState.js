bdcc759056280f96cbd64b8c81d94873fe1acd08
palashhedau <palashhedau900@gmail.com>
2017-11-20 21:01:39 -0800

    Added mongoose functionality and bug fixes

--- admin_frontend/src/reducers/initialState.js
+++ admin_frontend/src/reducers/initialState.js
@@ -1,22 +1,37 @@
-
 export default {
   isLogged: undefined,
   username: "",
-  
-  // Cars Initial Data
+
+  //cars
   allCars : [] ,
   carAddSuccess : null ,
   carUpdateSuccess : null,
   carDeleteSuccess : null ,
   currentCarToUpdate : {
-  							carQuantity : 0 ,
-  							carType : '',
-  							carName : '',
-  							occupancy : '',
-  							luggage : "NO" ,
-  							dailyRentalValue : 0,
-                serviceStartDate : '',
-                serviceEndDate : ''
-  						}
+    carQuantity : 0 ,
+    carType : '',
+    carName : '',
+    occupancy : '',
+    luggage : "NO" ,
+    dailyRentalValue : 0,
+    serviceStartDate : '',
+    serviceEndDate : ''
+  }
   //Cars Initial Data End
+
+  
+  allHotels : [] ,
+  hotelAddSuccess : null ,
+  hotelUpdateSuccess : null,
+  currentHotelToUpdate : {
+    hotelName : '',
+    hotelAddress : '', 
+    hotelCity : '',
+    hotelState : '', 
+    hotelZip : '',
+    hotelStar : '',
+    hotelRating : '',
+    hotelPhoneNumber : '',
+    hotelEmail : ''
+  }
 }
