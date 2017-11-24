054d45545c262b2d70244d67b0219a4d99634a0e
palashhedau <palashhedau900@gmail.com>
2017-11-24 15:25:58 -0800

    Corrected of Car Fetching to the listing file

--- frontend/src/components/listings/carRow/carRow.js
+++ frontend/src/components/listings/carRow/carRow.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import './carRow.css';
 import 'react-rater/lib/react-rater.css';
-import CarRowComponent from './carRowComponent/carRowComponent';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import { getAllCars } from '../../../actions/car';
+import Rater from 'react-rater' ;
+import './carRow.css';
 
 class CarRow extends Component {
 
-	componentWillMount(){
-		this.props.getAllCars() ; 
-	}
-
+	
     render() {
     	return (
            <div>
@@ -22,7 +20,45 @@ class CarRow extends Component {
            			:
 
            			this.props.allCars.map((car , key) => {
-           				return <CarRowComponent car={car} key={key} />
+           				return  <div className="cars-row" key={key}>
+                              <div className="info-section">
+                                  
+                                
+                                
+                                <div className="divForImageAndName row">
+                                  
+                                    <div className="carName-category col-lg-7 col-md-7 col-sm-7 col-xs-7">
+                                      
+                                        <Rater total={5} rating={4} interactive={false}/>
+                                      
+                                      <div className="category-section">
+                                        {car.carType}
+                                      </div>
+                                      <div>{car.carName} or Similar</div>
+                                      <div className="excess-info-text">
+                                        <span className="occupancy"><i className="fa fa-user fa-lg userFont" aria-hidden="true"></i>{car.occupancy}</span>
+                                        <span className="luggage"><i className="fa fa-suitcase fa-lg suitcaseFont" aria-hidden="true"></i>{car.luggage}</span>
+                                        <span className="availableCars"><i className="fa fa-car fa-lg carFont" aria-hidden="true"></i>{car.carQuantity}</span>
+                                      </div>
+                                      <hr></hr>
+                                      <div className="specialRateButtonDiv">
+                                        <button className="btn btn-default specialRateButton">Special Rate</button>
+                                      </div>
+                                    </div>
+                                    <div className="carImageDiv col-lg-5 col-md-5 col-sm-5 col-xs-5">
+                                      <img className="carImage" src="/assets/images/hyundai.png"
+                                        alt="Vehicle type: Economy - Hyundai Accent or similar"/>
+                                    </div>
+                                    
+                        </div>
+                      </div>
+                              
+                              <div className="price-section">
+                        <p>${car.dailyRentalValue}</p>
+                        <button className="btn btn-primary btn-kayak">Book</button>
+                      </div>
+
+                          </div>
            			})
            		}
 
