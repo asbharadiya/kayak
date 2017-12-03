25df49b19d4228e2e29bd9baff29817a8c76fd9a
Maulik Bhatt <bhattmaulik1991@gmail.com>
2017-12-03 03:22:52 -0800

    user analytics clicks and views

--- admin_frontend/src/reducers/userTracking.js
+++ admin_frontend/src/reducers/userTracking.js
@@ -3,14 +3,12 @@ import initialState from './initialState';
 const reducer = (state = initialState, action) => {
   	switch (action.type) {
         case "GET_USER_ANALYTICS_SUCCESS" :
-        	var userAnalytics = [];
-        	for (var key in action.payload[0].results.clicksPerPage) {
-        		userAnalytics.push({name: key, value: action.payload[0].results.clicksPerPage[key].length});
-        	}
-        	console.log(userAnalytics);
             return {
                 ...state,
-                userAnalytics : userAnalytics
+                userAnalyticsPageClicks : action.payload.userAnalyticsPageClicks,
+                userAnalyticsListingViewCar : action.payload.userAnalyticsListingViewCar,
+                userAnalyticsListingViewHotel : action.payload.userAnalyticsListingViewHotel,
+                userAnalyticsListingViewFlight : action.payload.userAnalyticsListingViewFlight
             };
         case "GET_USER_ANALYTICS_FAILURE" :
             return {
